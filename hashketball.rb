ca66ca4b95540d6741c59ecc6ec19c262de9f040
lior Shine <shinelior@gmail.com>
2020-05-06 18:00:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -114,7 +114,7 @@ def game_hash
   game_hash[:away][:players][3][:rebounds] = 3
   game_hash[:away][:players][3][:assists] = 2 
   game_hash[:away][:players][3][:steals] = 1
-  game_hash[:away][:players][3][:Blocks]  = 1
+  game_hash[:away][:players][3][:blocks]  = 1
   game_hash[:away][:players][3][:Slam_Dunks] = 0
   game_hash[:away][:players][4] ={}
   game_hash[:away][:players][4][:player_name] ="Kemba Walker"
