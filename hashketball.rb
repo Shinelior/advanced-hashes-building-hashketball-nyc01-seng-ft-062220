68c0e51d14edde391855ac6a4948b167ea4e8777
lior Shine <shinelior@gmail.com>
2020-05-06 17:59:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -83,7 +83,7 @@ def game_hash
   game_hash[:away][:players][0][:points] = 10
   game_hash[:away][:players][0][:rebounds] = 1
   game_hash[:away][:players][0][:assists] = 1
-  game_hash[:away][:players][0][:Steals] = 2
+  game_hash[:away][:players][0][:steals] = 2
   game_hash[:away][:players][0][:Blocks] = 7
   game_hash[:away][:players][0][:Slam_Dunks] = 2
   game_hash[:away][:players][1] = {}
